84019770adad79dc72c368699d59d14f15670203
T-Deepthy <deeps1929@gmail.com>
2019-07-10 17:31:33 +0530

    Add authentication and cart using firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDEIaJM2gjfA7xrmN-RJmkl89o9u08jedI',
+    authDomain: 'meal-prep-3bac8.firebaseapp.com',
+    databaseURL: 'https://meal-prep-3bac8.firebaseio.com',
+    projectId: 'meal-prep-3bac8',
+    storageBucket: '',
+    messagingSenderId: '232975506715',
+    appId: '1:232975506715:web:560506397a1ccd37'
+};
+firebase.initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from '@/firebase/';
 
 Vue.config.productionTip = false;
 
